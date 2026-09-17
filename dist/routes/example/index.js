c1f6b9c6072bda130379573782a5f83e320b3858
Pablo Lopes <pablolopes.lopes@gmail.com>
2026-09-17 13:18:20 +0200

    del: remove dist from git

--- dist/routes/example/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const exampleRoute = async function (fastify) {
-    fastify.get('/', async function () {
-        return 'this is an example';
-    });
-};
-module.exports = exampleRoute;
--- dist/routes/root.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const rootRoute = async function (fastify) {
-    fastify.get('/', async function () {
-        return { root: true };
-    });
-};
-module.exports = rootRoute;
